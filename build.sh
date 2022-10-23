f2d036cceb653998a794c920a5e77ed354ed306b
bilizoekdev <115386314+bilizoekdev@users.noreply.github.com>
2022-10-23 14:24:38 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-corvus-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b ginkgo-arcana-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_whyred-userdebug
+lunch aosp_ginkgo-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=jenkins
@@ -13,5 +13,5 @@ export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=jenkins
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka corvus -j8
-# build CorvusOS 14
+make bacon-j8
+# build Arcana GALE
