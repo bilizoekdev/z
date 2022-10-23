789b4b5a3ce4218fd52614c33cb73d36824d32f8
Saitama <115386314+saitama-sense@users.noreply.github.com>
2022-10-23 18:25:32 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/saitama-sense/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b queso -g default,-mips,-darwin,-notdefault
+git clone https://github.com/saitama-sense/local_manifest --depth 1 -b 10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_ginkgo-userdebug
+lunch bootleg_whyred-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=Saitama
 export KBUILD_BUILD_HOST=Cirrus
@@ -13,5 +13,5 @@ export BUILD_USERNAME=Saitama
 export BUILD_HOSTNAME=Cirrus
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-make bacon-j8
-# build Arcana GALE
+mka bacon-j8
+# build bootleggers
